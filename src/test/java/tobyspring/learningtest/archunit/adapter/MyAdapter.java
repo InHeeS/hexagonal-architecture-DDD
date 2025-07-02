57717ea7d9e70136e4e57b80b1dd6cc2b0a46815
Inhee <whdlsgml1599@gmail.com>
2025-07-02 18:36:44 +0900

    MyAdapter.java 코드 수정

--- src/test/java/tobyspring/learningtest/archunit/adapter/MyAdapter.java
+++ src/test/java/tobyspring/learningtest/archunit/adapter/MyAdapter.java
@@ -4,4 +4,10 @@ import tobyspring.learningtest.archunit.application.MyService;
 
 public class MyAdapter {
     MyService myService;
+
+    void run(){
+        myService = new MyService();
+        System.out.println(myService);
+    }
+
 }
--- src/test/java/tobyspring/learningtest/archunit/domain/MyMember.java
+++ src/test/java/tobyspring/learningtest/archunit/domain/MyMember.java
@@ -3,5 +3,4 @@ package tobyspring.learningtest.archunit.domain;
 import tobyspring.learningtest.archunit.application.MyService;
 
 public class MyMember {
-    MyService myService;
 }
